7166fe459153b259a3d4fa099ea3f084d9877930
27Onion Nebell <zzy20080201@gmail.com>
2025-09-04 17:46:19 +0800

    fix&feat: fix some naming convention and add reload command

--- js/keycheck.js
+++ js/keycheck.js
@@ -1,6 +1,6 @@
 import pageData from "./page-data.js";
 
-function findSeq(keySeq) {
+function find_seq(keySeq) {
     if (pageData.keySequence.length < keySeq.length) return;
     for (let i = 0; i < keySeq.length; i++) {
         if (keySeq[keySeq.length-i-1] != pageData.keySequence[pageData.keySequence.length-i-1]) {
@@ -12,22 +12,27 @@ function findSeq(keySeq) {
 
 function keycheck_onion27() {
     const name = ['2', '7', 'o', 'n', 'i', 'o', 'n'];
-    if (findSeq(name)) {
+    if (find_seq(name)) {
         document.querySelector("[data-name]").style.textDecoration = "underline";
     }
 }
 
 function keycheck_benzyl_titanium() {
     const name = ['b', 'e', 'n', 'z', 'y', 'l'];
-    if (findSeq(name)) {
+    if (find_seq(name)) {
         document.querySelector("#benzyl-titanium").innerText = "2enzyl 7itanium";
     }
 }
 
 function keycheck_konami_seq() {
-    console.log(pageData.keySequence);
-    if (findSeq(['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a'])) {
+    if (find_seq(['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a'])) {
         document.querySelector("#output").innerText = "You just used the code, right? ";
+    }
+}
+
+function keycheck_reload() {
+    if (find_seq(['p', 'a', 'r', 'u', 'Enter']) || find_seq(['p', 'a', 'r', 'u', ' ', '-', 'Shift', 'S', 'y', 'u', 'Enter'])) {
+        location.reload(true);
     }
 }
 
@@ -35,5 +40,6 @@ export default [
     keycheck_onion27,
     keycheck_benzyl_titanium,
     keycheck_konami_seq,
+    keycheck_reload,
 ];
 
